1392119ace1fbb31195c9cc368c964297a3f8bf1
guigzoun <guillaume_gendre@hotmail.fr>
2020-10-20 21:03:14 +0200

    Début du projet 2

--- src/main/java/com/progweb/demo/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.progweb.demo;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Controller {
-
-}
